d9968ef9924d2d80469a4e922a48398dfaa97cc2
ahmedraed777 <araed2217@gmail.com>
2023-10-17 07:23:50 +0200

    A Modern Sass Folder Structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import vars from '../styles/style.module.scss';
 
 export default function Home() {
   return (
-    <main>
-      <div>
+    <main className={vars.grid}>
+      <div className={vars.title}>
         <h1>Hello from the other side</h1>
+        <h2>My name is Ahmed</h2>
       </div>
+      <div>Hi</div>
     </main>
   );
 }
---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <div>NavBar</div>
+      <nav>
+        <ul>
+          <li>one</li>
+          <li>two</li>
+          <li>three</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
